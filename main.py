d985e6d4e848113ea3ed846d51cba47df3a860fd
Mohannad-Ali <mohannad.ali.2.3.6.9@gmail.com>
2023-11-15 09:40:41 -0700

    generate your QR Code by Python

---
+++ main.py
@@ -0,0 +1,34 @@
+from tkinter import * 
+import qrcode 
+root = Tk() 
+root.title('QR Code Generator')
+root.geometry("1000x550")
+root.config(bg="#3f51b5")
+root.resizable(False, False) 
+#icon image 
+image_icon=PhotoImage(file="icon.png")
+root.iconphoto(False,image_icon)
+
+
+
+def generate():
+    name=title.get()
+    text=entry.get()
+    qr=qrcode.make(text)
+    qr.save("qrcodes/"+str(name)+".png")
+    global Image
+    Image=PhotoImage(file="qrcodes/"+str(name)+".png")
+    Image_view.config(image=Image)
+
+
+
+Image_view = Label(root , bg="#3f51b5")
+Image_view.pack(padx=50 , pady=10 , side=RIGHT)
+
+Label(root ,text='title' ,fg="white",bg="#AE2321", font=15).place(x=50,y=170)
+title=Entry(root,width= 13 ,font="arial 15")
+title.place(x=50,y=200)
+entry=Entry(root,width= 28 ,font="arial 15")
+entry.place(x=50,y=250)
+Button(root,text="Generate",width=20,height=2,bg="black",fg="white" ,command=generate).place(x=50,y=300)
+root.mainloop()
